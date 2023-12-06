d310bfd0ee6ab354d3e03a5e3619a31078dcfde2
joel <madushanjoel@.com>
2023-12-06 20:33:41 +0530

    edit on view screen

--- app/src/main/java/com/example/contactview/ContactDetailsActivity.java
+++ app/src/main/java/com/example/contactview/ContactDetailsActivity.java
@@ -2,20 +2,23 @@ package com.example.contactview;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.contactview.dto.Contact;
 import com.example.contactview.storage.ContactDatabaseHelper;
 
 public class ContactDetailsActivity extends Activity {
 ContactDatabaseHelper contactDatabaseHelper;
+    long id=-1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_details);
         contactDatabaseHelper=new ContactDatabaseHelper(this);
         Intent intent = getIntent();
-        long id = intent.getLongExtra("id",-1);
+         id = intent.getLongExtra("id",-1);
         Contact contact=contactDatabaseHelper.getContactById(id);
         TextView nameTextView = findViewById(R.id.nameTextView);
         TextView emailTextView = findViewById(R.id.emailTextView);
@@ -23,5 +26,14 @@ ContactDatabaseHelper contactDatabaseHelper;
         nameTextView.setText(contact.getName());
         emailTextView.setText(contact.getEmail());
         phoneTextView.setText(contact.getPhoneNumber());
+    }
+
+    public void edit(View view){
+        Intent intent = new Intent(this, EditContactActivity.class);
+
+        intent.putExtra("id", id);
+        //Toast.makeText(this, "ID:"+String.valueOf(contact.getId()), Toast.LENGTH_SHORT).show();
+
+        startActivityForResult(intent, 2);
     }
 }
